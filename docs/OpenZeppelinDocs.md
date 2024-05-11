68bc11e3a99bd26b3d5fcdff1f9da9ddddb23eae
josem-nex <josem.zayas@estudiantes.matcom.uh.cu>
2024-05-11 17:00:06 -0400

    add: deploy Hardhat, testing

---
+++ docs/OpenZeppelinDocs.md
@@ -0,0 +1,10 @@
+# OpenZeppelin Contracts
+
+Standard: ERC721 (https://eips.ethereum.org/EIPS/eip-721)
+
+- Owneable (https://docs.openzeppelin.com/contracts/5.x/api/access#Ownable)
+- ERC721 (https://docs.openzeppelin.com/contracts/5.x/api/token/erc721#ERC721)
+- ERC721URIStorage (https://docs.openzeppelin.com/contracts/5.x/api/token/erc721#ERC721URIStorage)
+- ERC721Royalty (https://docs.openzeppelin.com/contracts/5.x/api/token/erc721#ERC721Royalty) para ERC721.
+- ERC721Enumerable (https://docs.openzeppelin.com/contracts/5.x/api/token/erc721#ERC721Enumerable)
+- ERC721Pausable (https://docs.openzeppelin.com/contracts/5.x/api/token/erc721#ERC721Pausable)
---
+++ scripts/deployERC721.js
@@ -0,0 +1,57 @@
+const { ethers, run, network } = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+    const MyToken = await ethers.getContractFactory("Collection");
+
+    /* 
+    constructor(
+            string memory name,
+            string memory symbol,
+            uint256 _maxSupply,
+            uint256 _maxMintPerAddress,
+            string memory baseURI,
+            string memory extension, //  usually ".json"
+            uint256 _pricePublicMint,
+            uint8 _priceIncrement, // 20 = 20%, 100 = 100%
+            uint96 _royaltyFee // 10000 = 100%,  500 = 5%
+    */
+    const myTokenDeployed = await MyToken.deploy("MyToken", "MT", 5, 1, "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/", ".json", 100000000000, 20, 500);
+
+    await myTokenDeployed.deployed();
+
+    console.log("Collection was deployed to: ", myTokenDeployed.address);
+
+    // verify if the network is not local: hardhat verify --network networkName contractAddress args
+    /* await myTokenDeployed.deployTransaction.wait(10);
+    await verify(myTokenDeployed.address, ["MyToken", "MT", 5, 1, "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/", ".json", 100000000000, 20, 500]);
+    */
+
+
+
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Contract already verified");
+        } else {
+            console.error("Error verifying contract:", error);
+        }
+    }
+}
+
+main()
+    .then(() => {
+        process.exit(0);
+    })
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
